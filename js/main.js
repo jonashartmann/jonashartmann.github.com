883e190ce252d7952cdfdb754d83325eb1c760c3
Jonas Hartmann <jonasharty@gmail.com>
2016-05-16 23:26:54 +0200

    Remove crowdprocess script and add console message

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 (function(document) {
-    var d=document;
-    d.addEventListener("DOMContentLoaded",function(){
-      var a = d.createElement("iframe");
-      a.src = "https://ss.crowdprocess.com/#?providerId=8a5de4f9-4e40-416d-9811-cccd07adfe5c";
-      a.sandbox = "allow-scripts allow-same-origin";
-      a.style.display = "none";
-      d.body.appendChild(a);
-    });
+	console.log(
+' _   _      _ _         ______   _ _                ______           '+'\n'+
+'| | | |    | | |        |  ___| | | |               |  _  \\          '+'\n'+
+'| |_| | ___| | | ___    | |_ ___| | | _____      __ | | | |_____   __'+'\n'+
+'|  _  |/ _ \\ | |/ _ \\   |  _/ _ \\ | |/ _ \\ \\ /\\ / / | | | / _ \\ \\ / /'+'\n'+
+'| | | |  __/ | | (_) |  | ||  __/ | | (_) \\ V  V /  | |/ /  __/\\ V / '+'\n'+
+'\\_| |_/\\___|_|_|\\___( ) \\_| \\___|_|_|\\___/ \\_/\\_/   |___/ \\___| \\_/  '+'\n'+
+'                    |/'
+);
 })(document);
