19daf8590c80b41a3823bb415d911ff6089f9fcc
Jonas Hartmann <jonasharty@gmail.com>
2016-10-22 21:06:33 +0200

    Add SVG logo with animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function(window) {
+	'use strict';
 
 	var printMessage = function printMessage () {
 		console.log(
@@ -10,20 +11,49 @@
 		'\\_| |_/\\___|_|_|\\___( ) \\_| \\___|_|_|\\___/ \\_/\\_/   |___/ \\___| \\_/  '+'\n'+
 		'                    |/'
 		);
-		console.log('Reply @ https://twitter.com/jonashart');
+		console.log('Say hi @ https://twitter.com/jonashart');
 	};
 
-	$(function () {
-		var elem = $('.mdl-layout');
-		elem[0].addEventListener('mdl-componentupgraded', function (e) {
-			// printMessage();
-			$('html').removeClass('loading');
-		});
-
-		var height = 1000;
-		var width = 1024;
+	var initTechRadar = function initTechRadar (height, width) {
 		var techRadar = new window.Radar(height, width);
 		var data = window.radarData;
 		techRadar.render(data.prepare(height, width));
+	};
+
+	function finishedAnimation(vivusEl) {
+		// console.log('Finished.', vivusEl);
+	}
+	var initLogo = function initLogo (element, start) {
+		return new Vivus(element,
+			{
+				duration: 200,
+				file: 'images/logo1.svg',
+				start: start || 'inViewport'
+			},
+			finishedAnimation);
+	};
+
+	$(function () {
+		var logo = initLogo($('.profile-logo')[0], 'manual');
+		var mobileLogo = initLogo($('.profile-logo')[1], 'manual');
+
+		var replayLogo = function replayLogo () {
+			logo.reset().play();
+		};
+
+		var elem = $('.mdl-layout');
+		elem[0].addEventListener('mdl-componentupgraded', function (e) {
+			if ($(e.target).hasClass('mdl-layout')) {
+				$('html').removeClass('loading');
+				setTimeout(replayLogo, 1);
+
+				var $drawerButton = $('.mdl-layout__drawer-button');
+				$drawerButton[0].addEventListener('click', function (e) {
+					mobileLogo.reset().play();
+				});
+			}
+		});
+
+		initTechRadar(1000, 1024);
 	});
 })(window);
